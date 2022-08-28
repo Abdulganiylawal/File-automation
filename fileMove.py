43554d0dfd3021ab64e7ef37f8e3b61f287a281f
Lawal Abdulganiy <labdulghaneey@gmail.com>
2022-08-28 13:13:18 +0100

    first commit

---
+++ fileMove.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+# creating a file using python
+directory = "{}"
+parentDirectory = "/Users/lawalabdulganiy/Downloads"
+
+sourceFolder = "/Users/lawalabdulganiy/Downloads"
+destinationFolder = "/Users/lawalabdulganiy/Downloads"
+
+myFiles = {'pdf': 'Pdf File', 'png': 'Image File', 'svg': 'Svg file',
+           'jpeg': 'Image File', 'pkt': 'Packet Tracer File', 'dmg': 'Application File', 'mp4': 'Movies File', 'docx': 'word Files', 'ppt': 'powerpoint File', 'xlsx': 'Excel FIles', 'pptx': 'powerpoint File', 'zip': 'Zipped Files', '7z': 'Zipped Files', }
+
+
+def File_Move(myFiles):
+    for fileName in os.listdir(sourceFolder):
+        for el in myFiles.keys():
+            if fileName.endswith(el):
+                source = os.path.join(sourceFolder, fileName)
+                destination = os.path.join(
+                    destinationFolder, myFiles[el])
+                shutil.move(source, destination)
+                print(f"{fileName} has moved")
+
+
+for el in myFiles.keys():
+    path = os.path.join(parentDirectory, directory.format(myFiles[el]))
+    print(path)
+    if os.path.exists(path):
+        File_Move(myFiles)
+        continue
+    os.mkdir(path)
+    File_Move(myFiles)
